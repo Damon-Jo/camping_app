5ff8716a805dbf5efb9f95545d4e90ddbc8d3469
Damon-Jo <lunaticjky@gmail.com>
2023-08-10 00:37:53 -0700

    added some APIs such as get request by id, post request for add new camp ground, and created some pages for frontend

--- app.js
+++ app.js
@@ -19,8 +19,7 @@ const app = express();
 
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname,'views'))
-
+app.set('views', path.join(__dirname,'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
@@ -33,6 +32,24 @@ app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds })
 });
+
+app.get('/campgrounds/new', (req,res)=>{
+    res.render('campgrounds/new');
+})
+
+app.post('/campgrounds', async(req, res)=>{
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+});
+
+app.get('/campgrounds/:id', async(req, res)=>{
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show', { campground })
+    // await Campground.findById(id: )
+}) ;
+
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
