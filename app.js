df9b340594d75a5dc3f2b85019b6bf73d9e5ce79
Damon-Jo <lunaticjky@gmail.com>
2023-08-24 18:09:25 -0700

    added currentUser helper function, and fixed the register route and added fucntion to return previous page

--- app.js
+++ app.js
@@ -63,6 +63,7 @@ passport.deserializeUser(User.deserializeUser());
 
 //define middleware to use flash befor routes
 app.use((req,res, next)=>{
+    res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
