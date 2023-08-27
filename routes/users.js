52fdbf0e258b62805041d270a3655e24ce1af139
Damon-Jo <lunaticjky@gmail.com>
2023-08-26 22:35:59 -0700

    Adjusted logic to correctly redirect user back to previous page after successful login from 'Edit' and 'New' buttons

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
 const passport = require('passport');
+const { storeReturnTo } = require('../middleware');
 
 router.get('/register', (req, res) => {
     res.render('users/register');
@@ -28,30 +29,24 @@ router.get('/login', (req, res) => {
     res.render('users/login');
 });
 
-router.post('/login', passport.authenticate('local', {failureFlash:true, failureRedirect:'/login'}), (req, res)=>{
-     req.flash('success', 'Welcome back!');
-     res.redirect('/campgrounds');
-});
+router.post('/login',
+    // use the storeReturnTo middleware to save the returnTo value from session to res.locals
+    storeReturnTo,
+    // passport.authenticate logs the user in and clears req.session
+    passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}),
+    // Now we can use res.locals.returnTo to redirect the user after login
+    (req, res) => {
+        req.flash('success', 'Welcome back!');
+        const redirectUrl = res.locals.returnTo || '/campgrounds'; // update this line to use res.locals.returnTo now
+        res.redirect(redirectUrl);
+    });
 
-
-// router.post('/login',
-//     // use the storeReturnTo middleware to save the returnTo value from session to res.locals
-//     storeReturnTo,
-//     // passport.authenticate logs the user in and clears req.session
-//     passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}),
-//     // Now we can use res.locals.returnTo to redirect the user after login
-//     (req, res) => {
-//         req.flash('success', 'Welcome back!');
-//         const redirectUrl = res.locals.returnTo || '/campgrounds'; // update this line to use res.locals.returnTo now
-//         res.redirect(redirectUrl);
-//     });
-
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
-    req.flash('success', 'welcome back!');
-    const redirectUrl = req.session.returnTo || '/campgrounds';
-    delete req.session.returnTo;
-    res.redirect(redirectUrl);
-})
+// router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+//     req.flash('success', 'welcome back!');
+//     const redirectUrl = req.session.returnTo || '/campgrounds';
+//     delete req.session.returnTo;
+//     res.redirect(redirectUrl);
+// })
 
 
 router.get('/logout', (req, res, next) => {
