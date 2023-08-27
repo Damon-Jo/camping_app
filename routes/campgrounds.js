d34d13d06e474e1770ccbe0a87bbd6272dd48868
Damon-Jo <lunaticjky@gmail.com>
2023-08-26 22:07:23 -0700

    added the logic for review authorization

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
-
 const Campground = require('../models/campground');
-
 const {isLoggedIn, isAuthor, validateCampground} = require('../middleware');
-
-
 
 router.get('/', catchAsync(async (req, res) => {
     const campgrounds = await Campground.find({});
@@ -29,7 +25,12 @@ router.post('/', isLoggedIn, validateCampground, catchAsync(async(req, res, next
 }));
 
 router.get('/:id', catchAsync(async(req, res)=>{
-    const campground = await Campground.findById(req.params.id).populate('reviews').populate('author');
+    const campground = await Campground.findById(req.params.id).populate({
+        path: 'reviews',
+        populate: {
+            path: 'author'
+        }
+    }).populate('author');
     console.log(campground);
     if(!campground){
         req.flash('error', 'Cannot find that campground!');
