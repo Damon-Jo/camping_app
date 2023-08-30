004a9a7a46f31445f6f4280782b742ee0c39a8f4
Damon-Jo <lunaticjky@gmail.com>
2023-08-30 00:25:52 -0700

    implemented the deleting image function from Backend

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,4 +1,5 @@
 const Campground = require('../models/campground');
+const { cloudinary } = require('../cloudinary');
 
 module.exports.index = async (req, res) => {
     const campgrounds = await Campground.find({});
@@ -52,6 +53,7 @@ module.exports.updateCampground = async (req, res) => {
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
     campground.images.push(...imgs);
+
     await campground.save();
     if (req.body.deleteImages) {
         for (let filename of req.body.deleteImages) {
