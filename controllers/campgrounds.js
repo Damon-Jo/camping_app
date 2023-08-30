1bd55df498855a2c6cc64a5c7051f8030cd0afcd
Damon-Jo <lunaticjky@gmail.com>
2023-08-29 20:15:39 -0700

    fixed the editing function for uploading img

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -46,9 +46,19 @@ module.exports.renderEditForm = async(req, res)=>{
     res.render('campgrounds/edit', { campground })
 }
 
-module.exports.updateCampground = async(req, res)=>{
+module.exports.updateCampground = async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+    console.log(req.body);
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
+    campground.images.push(...imgs);
+    await campground.save();
+    if (req.body.deleteImages) {
+        for (let filename of req.body.deleteImages) {
+            await cloudinary.uploader.destroy(filename);
+        }
+        await campground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } })
+    }
     req.flash('success', 'Successfully updated campground!');
     res.redirect(`/campgrounds/${campground._id}`)
 }
